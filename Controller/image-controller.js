ee84dd110f52c48c4dcb7bd42f13a744991e992f
Tinu  kumar <77734579+Tinu01kumar@users.noreply.github.com>
2023-03-26 21:49:39 +0530

    url added

--- Controller/image-controller.js
+++ Controller/image-controller.js
@@ -1,7 +1,7 @@
 import grid from 'gridfs-stream';
 import mongoose from 'mongoose';
 
-const url = 'http://localhost:8000';
+const url = 'https://blog-backend-zx1l.onrender.com';
 
 
 let gfs, gridfsBucket;
